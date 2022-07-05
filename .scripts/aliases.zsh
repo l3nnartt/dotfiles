f219c5c944147b817e041f87d84e13c908246ef1
Lennart Lösche <loesche.lennart@gmail.com>
2022-07-05 16:38:45 +0200

    upstream

--- .scripts/aliases.zsh
+++ .scripts/aliases.zsh
@@ -15,6 +15,7 @@ alias "node1"="ssh root@46.38.243.138 -L 6443:46.38.243.138:6443"
 alias "node2"="ssh root@193.26.157.104 -L 6443:193.26.157.104:6443"
 alias "node3"="ssh root@89.58.31.131 -L 6443:89.58.31.131:6443"
 alias "devnet"="ssh root@devnet-bigmac.timolia.de"
+alias "update"="paru -Syyu"
 
 man() {
     LESS_TERMCAP_md=$'\e[01;31m' \
